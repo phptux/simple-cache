8f2b5148bcaebc6291cc53be3dd96fe5b0139840
phptux <php.tux@web.de>
2021-01-05 20:33:06 +0100

    Update to PHP 8

--- src/CacheInterface.php
+++ src/CacheInterface.php
@@ -1,6 +1,9 @@
 <?php
+declare(strict_types=1);
 
 namespace Psr\SimpleCache;
+
+use DateInterval;
 
 interface CacheInterface
 {
@@ -15,14 +18,14 @@ interface CacheInterface
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *   MUST be thrown if the $key string is not a legal value.
      */
-    public function get(string $key, $default = null);
+    public function get(string $key, mixed $default = null): mixed;
 
     /**
      * Persists data in the cache, uniquely referenced by a key with an optional expiration TTL time.
      *
-     * @param string                 $key   The key of the item to store.
-     * @param mixed                  $value The value of the item to store, must be serializable.
-     * @param null|int|\DateInterval $ttl   Optional. The TTL value of this item. If no value is sent and
+     * @param string                $key    The key of the item to store.
+     * @param mixed                 $value  The value of the item to store, must be serializable.
+     * @param null|int|DateInterval $ttl    Optional. The TTL value of this item. If no value is sent and
      *                                      the driver supports TTL then the library may set a default value
      *                                      for it or let the driver take care of that.
      *
@@ -31,7 +34,7 @@ interface CacheInterface
      * @throws \Psr\SimpleCache\InvalidArgumentException
      *   MUST be thrown if the $key string is not a legal value.
      */
-    public function set(string $key, $value, $ttl = null): bool;
+    public function set(string $key, mixed $value, null|int|DateInterval $ttl = null): bool;
 
     /**
      * Delete an item from the cache by its unique key.
@@ -64,13 +67,13 @@ interface CacheInterface
      *   MUST be thrown if $keys is neither an array nor a Traversable,
      *   or if any of the $keys are not a legal value.
      */
-    public function getMultiple(iterable $keys, $default = null): array;
+    public function getMultiple(iterable $keys, mixed $default = null): array;
 
     /**
      * Persists a set of key => value pairs in the cache, with an optional TTL.
      *
-     * @param iterable               $values A list of key => value pairs for a multiple-set operation.
-     * @param null|int|\DateInterval $ttl    Optional. The TTL value of this item. If no value is sent and
+     * @param iterable              $values  A list of key => value pairs for a multiple-set operation.
+     * @param null|int|DateInterval $ttl     Optional. The TTL value of this item. If no value is sent and
      *                                       the driver supports TTL then the library may set a default value
      *                                       for it or let the driver take care of that.
      *
@@ -80,7 +83,7 @@ interface CacheInterface
      *   MUST be thrown if $values is neither an array nor a Traversable,
      *   or if any of the $values are not a legal value.
      */
-    public function setMultiple(iterable $values, $ttl = null): bool;
+    public function setMultiple(iterable $values, null|int|DateInterval $ttl = null): bool;
 
     /**
      * Deletes multiple cache items in a single operation.
